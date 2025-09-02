c598dffaed1bd819b4eabd2dd837ca7bf67ed86b
EdsonRodrigues <edsonrodrigues111@hotmail.com>
2025-09-02 16:05:54 -0300

    Primeiro commit do projeto

---
+++ cartas-personagens-edfeec1395c2708f8b1b7b7370b6226658b976f3/src/modules/arqueiro-mago.js
@@ -0,0 +1,17 @@
+import { Arqueiro } from "./arqueiro.js";
+import { Mago } from "./mago.js";
+import { Personagem } from "./personagem.js";
+
+export class ArqueiroMago extends Personagem{
+ladoArqueiro
+ladoMago
+static tipo = 'ArqueiroMago'
+constructor(nome, destreza, elementoMagico, levelMagico, inteligencia){
+	super(nome)
+	this.ladoArqueiro= new Arqueiro(nome, destreza)
+	this.ladoMago = new Mago (nome, elementoMagico, levelMagico, inteligencia)
+}
+obterInsignia(){
+	return `${this.ladoArqueiro.obterInsignia()} e ${ this.ladoMago.obterInsignia()}`
+}
+}
---
+++ cartas-personagens-edfeec1395c2708f8b1b7b7370b6226658b976f3/src/modules/arqueiro.js
@@ -0,0 +1,16 @@
+import { Personagem } from "./personagem.js";
+
+export class Arqueiro extends Personagem {
+	static tipo =  'Arqueiro'
+	destreza
+constructor(nome, destreza){
+	super(nome)
+	this.destreza= destreza
+}
+obterInsignia(){
+	if( this.destreza>= 5 ){
+		return 'Dominador de flexas'
+	}
+	return super.obterInsignia()
+}
+}
